dd1d66d944dab04f18c507abdf10537631080265
Ali Rakhshanipur <alirakhshanipur@Alis-MacBook-Pro.local>
2022-03-31 15:13:56 +0430

    onContextMenu Project

---
+++ js/script.js
@@ -0,0 +1,20 @@
+﻿let $ = document;
+let contextMenu = $.getElementById("contextMenu");
+let body = $.getElementsByTagName("body")[0];
+
+body.addEventListener("click", () => {
+  contextMenu.style.display = "none";
+});
+body.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    contextMenu.style.display = "none";
+  }
+});
+
+body.addEventListener("contextmenu", (event) => {
+  event.returnValue = false;
+  contextMenu.style.display = "block";
+  contextMenu.style.left = event.pageX;
+  contextMenu.style.top = event.pageY;
+  //   console.log(event);
+});
